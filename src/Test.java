a329f1c9335dca515824cc13b49ccb76f01a6e62
momochen <momochenonline@gmail.com>
2013-03-18 22:05:05 -0400

    First Commit
    Test with hello world for fun

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author cheny18.
+ *         Created Mar 18, 2013.
+ */
+public class Test {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+		System.out.println("Hello world");
+	}
+
+}
